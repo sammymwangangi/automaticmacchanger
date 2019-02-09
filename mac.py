066088f0ab5ffc0ae0f105c8acd40d8ab4a63ea6
Sammy Mwangangi <sammymwinzi2@gmail.com>
2019-02-09 17:14:03 +0300

    set wlan0 as default

--- mac.py
+++ mac.py
@@ -16,8 +16,8 @@ time.sleep(3)
 
 while True:
     os.system("service network-manager stop")
-    os.system("ifconfig eth0 down")
-    print(os.popen("macchanger eth0 -r").read())
-    os.system("ifconfig eth0 up")
+    os.system("ifconfig wlan0 down")
+    print(os.popen("macchanger wlan0 -r").read())
+    os.system("ifconfig wlan0 up")
     os.system("service network-manager start")
     time.sleep(300)
